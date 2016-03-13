fca19ce466d1f2bc24be22a8d82250594bd85c0f
charlesdrews <charlesdrews@gmail.com>
2016-03-13 00:24:06 -0500

    Implement speech recognition & send result to beer search api

--- QuickTap/app/src/main/java/com/example/android/quicktap/BreweryDbApi/Beer.java
+++ QuickTap/app/src/main/java/com/example/android/quicktap/BreweryDbApi/Beer.java
@@ -1,10 +1,13 @@
 package com.example.android.quicktap.BreweryDbApi;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
 /**
  * Created by charlie on 3/12/16.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Beer {
     private String name, nameDisplay;
     private Style style;
@@ -21,5 +24,21 @@ public class Beer {
             return breweries.get(0);
         }
         return null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNameDisplay(String nameDisplay) {
+        this.nameDisplay = nameDisplay;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+
+    public void setBreweries(List<Brewery> breweries) {
+        this.breweries = breweries;
     }
 }
--- QuickTap/app/src/main/java/com/example/android/quicktap/BreweryDbApi/Response.java
+++ QuickTap/app/src/main/java/com/example/android/quicktap/BreweryDbApi/Response.java
@@ -1,12 +1,19 @@
 package com.example.android.quicktap.BreweryDbApi;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
 /**
  * Created by charlie on 3/12/16.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Response {
     private List<Beer> data;
 
     public List<Beer> getBeers() { return data; }
+
+    public void setData(List<Beer> data) {
+        this.data = data;
+    }
 }
